84d5a24bb47ea72a692dcd0ba1130c9502baea65
Suraj Bansal <bansalsuraj03@gmail.com>
2020-07-06 14:13:51 -0400

    fade effect. but better

--- index.js
+++ index.js
@@ -19,30 +19,26 @@ window.addEventListener('load', function(){
   overlay.style.display = 'none';
 })
 
-$(function(){  // $(document).ready shorthand
-  $('.showcase').fadeIn('slow');
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fade').each( function(i){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},250);       
+            }
+        });  
+    });
 });
 
 $(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.fade').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},250);
-                    
-            }
-            
-        }); 
-    
-    });
-    
+  $(window).scroll( function(){
+      $('.footer-fade').each( function(i){
+          var bottom_of_object = $(this).position().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          if( bottom_of_window > bottom_of_object ){
+              $(this).animate({'opacity':'1'},150);       
+          }
+      });  
+  });
 });
